c6d5a9be5560667b4a8ad833825458b0180fdb71
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-03-14 08:13:13 -0500

    Clase-14Marzo

---
+++ tareas/Taller4-Javascript/main.js
@@ -0,0 +1,4 @@
+
+console.log("%c¡Ejercicio 6 !",
+"background:linear-gradient(#000, #FF8000); color:#fff; padding: 5px 10px;");
+console.log("Construir el algoritmo en Javascript para un programa para cualquier cantidad de estudiantes que lea el nombre, el sexo y la nota definitiva y halle al estudiante con la mayor nota y al estudiante con la menor nota y cuantos eran hombres y cuantos mujeres.");
--- tareas/pruebas/main.js
+++ tareas/pruebas/main.js
@@ -1,2 +1,30 @@
-var Div = document.getElementsByTagName('div');
-console.log(Div);
+/*let myTbody = document.querySelector(".myTbody")
+let Nombre = document.querySelector(".Name");
+Nombre.innerHTML = null;
+
+let i = 0
+let data = [];
+
+do{
+    data.unshift(prompt(`Ingrese el datos ${data.length+1}`))
+}while(data.length<5)
+
+console.log(data);
+
+data.forEach((val, id) => {
+    myTbody.insertAdjacentHTML("beforeend", `
+        <tr>
+            <td>${id}</td>
+            <td>${val}</td>
+        </tr>
+    `);
+});
+
+Object.fromEntries(new FormData(Data));
+*/
+
+let btn = document.querySelector(".btn");
+btn.addEventListener("focus", (e)=>{
+    alert("sexppppp");
+})
+console.dir(btn)
