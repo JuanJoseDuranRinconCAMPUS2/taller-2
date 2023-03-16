0448c6722b4e9ca5ebd1b541fb83949595426f2b
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-03-16 09:26:10 -0500

    feat: :recycle: Clase-16marzo
    Clase-16marzo

--- tareas/Taller3-JavaScript/main.js
+++ tareas/Taller3-JavaScript/main.js
@@ -1,16 +1,18 @@
 let myFormularioCampus = document.querySelector("#myFormularioCampus");
 let myFormularioCampers = document.querySelector("#myFormularioCampers");
-let Rol = document.querySelector("[name='Rol']")
+let myFormularioHoras = document.querySelector("#myFormularioHoras");
+let Rol = document.querySelector("[name='Rol']");
 
 let campus = {};
 
 myFormularioCampus.addEventListener("submit", (e)=>{
     e.preventDefault();
     let data = Object.fromEntries(new FormData(e.target))
-    campus[`${data.nombreSede}`] = {Camper: [], Trainers: []};
+    campus[`${data.nombreSede}`] = {Camper: [], Trainers: [], Niveles : [], roadMap : []};
     listaSedes();
+    listaSedes1();
     myFormularioCampus.reset();
-})
+});
 
 let listaSedes = ()=>{
     let opciones = document.querySelector("[name='sede']");
@@ -22,18 +24,57 @@ let listaSedes = ()=>{
     }
 }
 
+let listaSedes1 = ()=>{
+    let opciones = document.querySelector("[name='sede1']");
+    opciones.innerHTML = null;
+    for (let [val, id] of Object.entries(campus)) {
+        opciones.insertAdjacentHTML("beforeend", `
+            <option value="${val}">${val}</option>
+        `);
+    }
+}
+
 myFormularioCampers.addEventListener("submit", (e)=>{
     e.preventDefault();
     let data = Object.fromEntries(new FormData(e.target));
+    let programa = document.querySelector("[name='clase']");
+    programa.innerHTML = null;
     console.log(data);
     let sede = data.sede;
     let rol = data.Rol;
-    delete data.sede;
     if(rol == "camper"){
         campus[`${sede}`]["Camper"].unshift(data);
+        programa.insertAdjacentHTML("beforeend", `
+        <option value="Programación">Programación</option>
+        <option value="Ingles">Ingles</option>
+        <option value="Ser">Ser</option>
+        `);
     }if (rol == "trainer"){
         campus[`${sede}`]["Trainers"].unshift(data);
+        programa.insertAdjacentHTML("beforeend", `
+        <option value="Programación">Programación</option>
+        `);
     }
+    console.log(data)
     console.log(campus);
     myFormularioCampers.reset();
+})
+
+myFormularioHoras.addEventListener("submit", (e)=>{
+    e.preventDefault();
+
+    let data = Object.fromEntries(new FormData(e.target));
+    let clase = data.clase;
+    let rol = data.Rol;
+    let sede1 = data.sede1;
+    delete data.rol;
+    campus[`${sede1}`]["Camper"].unshift(data);
+    if(rol == "camper"){
+        campus[`${sede1}`]["Camper"].unshift(data);
+    }if (rol == "trainer"){
+        campus[`${sede1}`]["Trainers"][`${clase}`].unshift(data);
+    }
+    console.log(campus);
+    console.log(data);
+    myFormularioHoras.reset();
 })
