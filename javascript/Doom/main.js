50a89301834220e4342cbd1e3d4ad6a9f1f84389
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-03-21 09:22:45 -0500

    Clase-21marzo

--- javascript/Doom/main.js
+++ javascript/Doom/main.js
@@ -1,13 +1,14 @@
 let div1 = document.querySelector(".box-1");
 let div2 = document.querySelector(".box-2");
-let div3 = document.querySelector("box-3");
+let div3 = document.querySelector(".box-3");
 
-let saludo = (e)=>{
-    alert("WENAS CTM");
-}
 
-div1.addEventListener("click", saludo);
+div1.addEventListener("click", function(e){
+    console.log("Caja1");
+});
 div2.addEventListener("click", function(e){
-    alert(e.target.dataset.opcion);
-}, {once : true});
-div3.addEventListener("click", saludo);
+    console.log("Caja2");
+});
+div3.addEventListener("click", function(e){
+    console.log("Caja3");
+});
